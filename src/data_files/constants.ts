04edfb34c69fe153dd96aff85236782c62f12337
reinaldo-martinez1 <reinaldo.martinez1@upr.edu>
2024-07-10 18:23:54 -0400

    Fixed image when sharing

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -2,9 +2,9 @@ import ogImageSrc from "@images/content/teamPhotos/moonBuggyLogoFixed.png";
 
 export const SITE = {
   title: "MoonBuggy Engineering Team",
-  tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
+  tagline: "MoonBuggy Engineering Team",
+  description: "Founded in 2012, our team is a dynamic and diverse group of students from various academic disciplines united by a passion for innovation and exploration. We bring together talents from engineering, business administration and other majors to design, build, and test human-powered rovers. Our collaborative spirit and multidisciplinary approach drive us to excel in the NASA Human Exploration Rover Challenge, pushing the boundaries of technology and teamwork as we prepare for the future of space exploration.",
+  description_short: "",
   url: "https://moon-buggy-website.vercel.app/",
   author: "Tasktool Department",
 };
@@ -13,7 +13,7 @@ export const SEO = {
   title: SITE.title,
   description: SITE.description,
   structuredData: {
-    "@context": "https://schema.org",
+    "@context": "https://moon-buggy-website.vercel.app/",
     "@type": "WebPage",
     inLanguage: "en-US",
     "@id": SITE.url,
